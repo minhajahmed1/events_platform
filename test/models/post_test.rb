270824b851f62ad29a5be565a4547d48c67ad640
Minhaj Ahmed <minhajahmed32@hotmail.com>
2018-05-08 19:54:54 +0000

    made some minor changes to app

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post=Post.new(:"ruby meetup")
+  end
+  
+  test "post should be valid" do
+    assert @post.valid?
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,30 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- test 'is valid' do
-    user = User.new( email: 'minhajahmed32@hotmail.com', user_id:'1', password: 'secret', password_confirmation: 'secret')
-    assert user.valid?
+  def setup
+    @user=User.new(email:"fo0@hotmail.com")
   end
   
-  test 'is invalid without a user_id' do
-    user= User.new(email:'minhajahmed32@hotmail.com')
-    reftute user.valid?
-    assert_not_nil user.erors[:user_id]
+  test "user should be valid" do
+    assert @user.valid?
   end
-  
- 
-
-  test 'is invalid without an email' do
-    user = User.new(user_id:'1')
-    refute user.valid?
-    assert_not_nil user.errors[:email]
-  end
-
-  test 'is invalid with an email of an existing user' do
-    user = User.new(user_id: '1', email: users(:one).email, password: 'secret', password_confirmation: 'secret')
-    refute user.valid?
-    assert_not_nil user.errors[:email]
-  end
-
-
 end
