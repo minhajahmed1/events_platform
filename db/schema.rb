6e1b07591492433dc2bf4bd0f7c75744e05c04fc
Ubuntu <ubuntu@ip-172-31-12-240.ca-central-1.compute.internal>
2018-07-02 19:22:54 +0000

    side is production ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522164254) do
+ActiveRecord::Schema.define(version: 20180413094453) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "date"
